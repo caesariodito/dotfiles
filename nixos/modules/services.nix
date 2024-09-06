5720df8c2310dec546b9a396fa099f4c59309688
NixOS - CD <caesariodito@gmail.com>
2024-09-06 21:23:25 +0700

    add(nixos): spotify and spicetify-cli packages

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -35,5 +35,13 @@
     vscode
     xdg-utils
     pass
+    spotify
+    spicetify-cli
   ];
+
+  # For sync local tracks spotify
+  networking.firewall.allowedTCPPorts = [ 57621 ];
+
+  # In order to enable discovery of Google Cast devices on spotify
+  networking.firewall.allowedUDPPorts = [ 5353 ];
 }
