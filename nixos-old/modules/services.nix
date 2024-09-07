36ba5664bd18e7a4133ea076a5d3961a15875ad6
NixOS - CD <caesariodito@gmail.com>
2024-09-07 09:44:47 +0700

    clean(nixos): remove unused stuff and add vesktop

--- nixos-old/modules/services.nix
+++ nixos-old/modules/services.nix
@@ -4,9 +4,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Enable flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
   # Enable Bluetooth
   hardware.bluetooth.enable = true;
 
@@ -19,21 +16,4 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
-
-  # Enable Docker
-  virtualisation.docker.enable = true;
-
-  # Install Firefox
-  programs.firefox.enable = true;
-
-  # System-wide packages
-  environment.systemPackages = with pkgs; [
-    wget
-    git
-    gh
-    vesktop
-    vscode
-    xdg-utils
-    pass
-  ];
 }
--- nixos/modules/home-manager/pkgs.nix
+++ nixos/modules/home-manager/pkgs.nix
@@ -6,5 +6,6 @@
   home.packages = with pkgs; [
     eza
     vscode
+    vesktop
   ];
 }
